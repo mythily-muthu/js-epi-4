4fc604e6461d1e71600241d9c47ac601a33da7d2
mythily-muthu <mythilimuthu18@gmail.com>
2022-10-12 17:23:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+//reference data types
+
+let a = {
+  key: 10,
+};
+let b = a;
+
+a.key = 20;
+
+let user = {
+  name: "mythu",
+  age: 22,
+  greeting() {
+    console.log(`my name is ${name}and iam now ${age} years old)`);
+  },
+};
+for (let key in user) console.log(user[key]);
+
+for (let key of Object.keys(user)) console.log("key:" + key);
+for (let key of Object.entries(user)) console.log("key:" + key);
