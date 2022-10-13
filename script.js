2ac3d9d88b4414a6720162b4fa3e3546c1b1ca69
mythily-muthu <mythilimuthu18@gmail.com>
2022-10-13 14:56:59 +0530

    added ui

--- script.js
+++ script.js
@@ -22,3 +22,45 @@ for (let key of Object.entries(user)) console.log("key:" + key);
 
 //js inbuild objects
 console.log(Math.abs(-3));
+
+// abs => turns (-) into (+) , ceil => gives 1 when given below 1
+console.log(Math.ceil(0.6));
+
+//Math
+
+let marks = [47, 78, 90, 57];
+
+let result = Math.min(...marks);
+console.log(result);
+
+//string object
+
+let name1 = "muthu";
+let name2 = "mani kandan ";
+
+/* 
+.concat 
+.includes
+.startswith
+.endswith
+.indexof
+.repeat
+.splice
+.touppercase
+.tolowercase
+.trim
+.trinstart
+/trimend
+
+escape notations
+new line => \n
+tab space => \t*/
+
+//template literals...
+let msg = `hi hello all
+ "pls" welcome..
+ please subscribe`;
+console.log(msg);
+
+//date object
+let;
